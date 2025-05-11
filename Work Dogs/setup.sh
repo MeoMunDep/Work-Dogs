29be34fab9c7fdac5c6ee7e85e69d82c120b379d
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-05-11 13:58:02 +0700

    update module for connect wallet

--- Work Dogs/setup.sh
+++ Work Dogs/setup.sh
@@ -77,7 +77,7 @@ print_green "Configuration files have been checked."
 
 print_yellow "Checking dependencies..."
 cd "$MODULES_DIR"
-npm install user-agents axios colors https-proxy-agent socks-proxy-agent 
+npm install user-agents axios colors https-proxy-agent socks-proxy-agent @ton/core
 cd - > /dev/null
 print_green "Dependencies installation completed!"
 
